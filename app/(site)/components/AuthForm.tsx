62587074b46642933ab9acb8d34ed485a7e48766
Mosquito <devesh27022001@gmail.com>
2023-06-21 11:12:04 +0530

    'last2'

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -140,19 +140,19 @@ const AuthForm = () => {
 
             <div className="relative flex justify-center text-sm">
 
-              <span className="bg-white px-2 text-gray-500">
-                Or Continue With
-              </span>
+              {/*<span className="bg-white px-2 text-gray-500">*/}
+              {/*  Or Continue With*/}
+              {/*</span>*/}
 
             </div>
 
           </div>
 
-          <div className="mt-6 flex gap-2">
+          {/*<div className="mt-6 flex gap-2">*/}
 
-            <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')}/>
+          {/*  <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')}/>*/}
 
-          </div>
+          {/*</div>*/}
 
         </div>
 
