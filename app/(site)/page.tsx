522bb724f43c5369c300211a2e8c5be68927712a
Mosquito <devesh27022001@gmail.com>
2023-06-22 10:10:22 +0530

    'fixedmobileviewprobmen'

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import '@/app/globals.css'
-
 import React, { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
 import AuthForm from "./components/AuthForm";
@@ -10,7 +8,6 @@ import { FiMoon, FiStar } from "react-icons/fi";
 import "./styles.css"; // Add your custom styles here
 
 const Auth = () => {
-
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowPandora(true);
@@ -28,38 +25,42 @@ const Auth = () => {
         config: { duration: 500 },
     });
 
+    const isMobileView = window.innerWidth < 768; // Adjust the breakpoint as per your needs
+
     return (
-        <div className="flex min-h-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500" >
+        <div className="flex min-h-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500">
             <div className="absolute top-0 right-0 z-10">
                 <FiStar className="animate-spin w-12 h-12 text-yellow-300" />
                 <FiMoon className="animate-pulse w-12 h-12 text-gray-300" />
             </div>
 
-            {/*<div className="absolute inset-0 flex flex-col justify-center items-center z-0">*/}
-            {/*    <Confetti*/}
-            {/*        width={window.innerWidth}*/}
-            {/*        height={window.innerHeight}*/}
-            {/*        recycle={false}*/}
-            {/*    />*/}
-            {/*</div>*/}
-
             <div className="w-full max-w-md flex flex-col justify-center py-12 px-6 z-10">
-                {/*<h2 className="text-3xl font-bold tracking-tight text-gray-900 text-center">*/}
-                {/*    Welcome to Pandora Chat*/}
-                {/*</h2>*/}
+                {isMobileView && (
+                    <animated.h1
+                        className="text-4xl font-bold text-yellow-300 mb-4"
+                        style={pandoraAnimation}
+                    >
+                        <span className="text-white">Apex</span>
+                        <span className="text-yellow-300">Horizon</span>
+                    </animated.h1>
+                )}
                 <AuthForm />
             </div>
-            <div className="flex flex-col justify-center items-start py-12 px-6 z-10">
-                <animated.h1
-                    className="text-4xl font-bold text-yellow-300 mb-4"
-                    style={pandoraAnimation}
-                >
-                    ApexHorizon
-                </animated.h1>
-                <animated.p className="text-white text-lg" style={pandoraAnimation}>
-                    ApexHorizon is a powerful and dynamic chat application designed to revolutionize the way people connect and communicate. With its cutting-edge features and user-friendly interface, Apex Connect offers a seamless and engaging messaging experience. Apex Connect boasts real-time messaging capabilities, allowing users to instantly exchange messages, photos and other media with their contacts. Whether its one-on-one conversations or group chats, ApexHorizon provides a platform for effortless and efficient communication.
-                </animated.p>
-            </div>
+
+            {!isMobileView && (
+                <div className="flex flex-col justify-center items-start py-12 px-6 z-10">
+                    <animated.h1
+                        className="text-4xl font-bold text-yellow-300 mb-4"
+                        style={pandoraAnimation}
+                    >
+                        <span className="text-green-300">Apex</span>
+                        <span className="text-yellow-300">Horizon</span>
+                    </animated.h1>
+                    <animated.p className="text-white text-lg" style={pandoraAnimation}>
+                        ApexHorizon is a powerful and dynamic chat application designed to revolutionize the way people connect and communicate. With its cutting-edge features and user-friendly interface, Apex Connect offers a seamless and engaging messaging experience. Apex Connect boasts real-time messaging capabilities, allowing users to instantly exchange messages, photos and other media with their contacts. Whether its one-on-one conversations or group chats, ApexHorizon provides a platform for effortless and efficient communication.
+                    </animated.p>
+                </div>
+            )}
         </div>
     );
 };
